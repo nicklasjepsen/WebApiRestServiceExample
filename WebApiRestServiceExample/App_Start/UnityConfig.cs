54e99a8c95eae49c81f5a1a447332b83f3bc88aa
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-12-29 11:37:31 +0100

    Configured Unity

--- WebApiRestServiceExample/App_Start/UnityConfig.cs
+++ WebApiRestServiceExample/App_Start/UnityConfig.cs
@@ -1,6 +1,8 @@
 using Microsoft.Practices.Unity;
 using System.Web.Http;
 using Unity.WebApi;
+using WebApiRestServiceExample.Controllers;
+using WebApiRestServiceExample.Providers;
 
 namespace WebApiRestServiceExample
 {
@@ -14,7 +16,9 @@ namespace WebApiRestServiceExample
             // it is NOT necessary to register your controllers
             
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+            container.RegisterType<IGoogleMapsProvider, GoogleMapsProvider>();
+            container.RegisterType<IDateTimeProvider, DateTimeProvider>();
+
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
